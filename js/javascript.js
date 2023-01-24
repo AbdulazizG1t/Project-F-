0f47c6dfb92ac73e9518084716827511fad8ec25
Abdulaziz <112411927+AbdulazizG1t@users.noreply.github.com>
2023-01-24 02:39:54 -0700

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,4 @@
-const API = `https://jsonplaceholder.typicode.com/comments`
+const API = `https://json/placeholder.typicode.com/comments`
 fetch(API)
     .then(response =>  response.json())
     .then(json => console.table(json))
